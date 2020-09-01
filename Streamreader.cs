598dc7763a5917b86dfa07e0d575aa95cc8cbc68
Artem Gorokhov <gorokhov.ar@gmail.com>
2020-09-01 22:10:31 +0300

    Add project files.

---
+++ Streamreader.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+using System.Text;
+using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
+namespace streamreader
+{
+    internal class Streamreader
+    {
+
+        static string GetFileText()
+        {           
+            FileStream stream = new FileStream("kl_to_1c_TEST.txt", FileMode.Open);
+            StreamReader reader = new StreamReader(stream, Encoding.Default, true);
+            string str = reader.ReadToEnd();
+            stream.Close();
+
+            return str;
+        }
+
+        static void Main()
+        {
+
+            string input = GetFileText();
+            
+            var Numberlist = Regex.Matches(input, "Дата=(.*)");
+            var Datalist = Regex.Matches(input, "Номер=(.*)");
+            var Sumlist = Regex.Matches(input, "Сумма=(.*)");
+
+
+     
+            // строка подключения к БД
+            string connStr = "server=localhost;user=root;database=people;password=0000;";
+            // создаём объект для подключения к БД
+            MySqlConnection conn = new MySqlConnection(connStr);
+            // устанавливаем соединение с БД
+            conn.Open();
+            // запрос
+            for (var i = 0; i < Numberlist.Count; i++)
+            {
+
+                Console.WriteLine(Numberlist[i].Groups[1].Value);
+                Console.WriteLine(Datalist[i].Groups[1].Value);
+                Console.WriteLine(Sumlist[i].Groups[1].Value);
+
+                string sql = $"INSERT INTO 'название таблицы' VALUES ({Numberlist[i].Groups[1].Value},{Datalist[i].Groups[1].Value},{Sumlist[i].Groups[1].Value});";
+                // объект для выполнения SQL-запроса
+                MySqlCommand command = new MySqlCommand(sql, conn);
+                // выполняем запрос и получаем ответ
+                string name = command.ExecuteScalar().ToString();
+            }
+            
+            // закрываем соединение с БД
+            conn.Close();
+
+            while (true) ;
+        }
+
+    }
+}
---
+++ regex/regex.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regex_3
+{
+
+    class Regex_3
+
+    {
+        static void Main()
+        {
+            char* buffer = Buffer;
+            string input = new string(buffer);
+
+            string pattern = @"\b(\d+\W?руб)";
+            Regex regex = new Regex(pattern);
+
+            // Получаем совпадения в экземпляре класса Match
+            Match match = regex.Match(input);
+
+            // отображаем все совпадения
+            while (match.Success)
+            {
+                // Т.к. мы выделили в шаблоне одну группу (одни круглые скобки),
+                // ссылаемся на найденное значение через свойство Groups класса Match
+                Console.WriteLine(match.Groups[1].Value);
+
+                // Переходим к следующему совпадению
+                match = match.NextMatch();
+            }
+        }
+    }
+}
+// using System;
+//using System.Text.RegularExpressions;
+
+//class Example
+//{
+//static void Main()
+//  {
+// Допустим в исходной строке нужно найти все числа,
+// соответствующие стоимости продукта
+//string input = "СекцияДокумент=Платежное поручение" +
+//"Дата=07.12.2010" +
+//"Номер=6" +
+//"Сумма=100.00" +
+//"ПлательщикСчет=40802810740000000001" +
+// "ДатаСписано=08.12.2010" +
+// "Плательщик=ИНН 644914186741/0  ИП Иванов И. И." +
+//"Плательщик1=ИП Иванов И. И. " +
+// "Плательщик2=40802810740000040746" +
+// "Плательщик3=Ф. ЗАО АКБ В Г.ЭНГЕЛЬСПлательщик4=Г. ЭНГЕЛЬС";
+
+// Regex regex = new Regex("Дата", RegexOptions.IgnoreCase);
+
+// Получаем совпадения в экземпляре класса Match
+// Match match = regex.Match(input);
+
+// отображаем все совпадения
+// while (match.Success)
+//  {
+// Т.к. мы выделили в шаблоне одну группу (одни круглые скобки),
+// ссылаемся на найденное значение через свойство Groups класса Match
+// Console.WriteLine(match.Groups[1].Value);
+
+// Переходим к следующему совпадению
+// match = match.NextMatch();
+// }
+//  Console.WriteLine();
+//  }
+
+//
